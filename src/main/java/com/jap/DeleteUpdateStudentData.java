f069a6f1795ccb1bce4828b8d6111a9bbe8d9f99
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-18 19:16:03 +0530

    reformatted the code

--- src/main/java/com/jap/DeleteUpdateStudentData.java
+++ src/main/java/com/jap/DeleteUpdateStudentData.java
@@ -1,36 +1,34 @@
 package com.jap;
 
-import java.sql.*;
+import java.sql.Connection;
 
 /**
  * Hello world!
- *
  */
-public class DeleteUpdateStudentData
-{
-
-    public Connection getConnection(){
-        Connection connection=null;
-
-        return connection;
-
-    }
-
-    public void deleteStudentData(){
-      // call getConnection() method
-       
-        //execute delete query
-    }
-
-    public void updateStudentData(){
-        // call getConnection() method
-
-        //execute update query
-    }
+public class DeleteUpdateStudentData {
 
     public static void main(String[] args) {
         DeleteUpdateStudentData salesDataDemo = new DeleteUpdateStudentData();
         salesDataDemo.deleteStudentData();
         salesDataDemo.updateStudentData();
+    }
+
+    public Connection getConnection() {
+        Connection connection = null;
+
+        return connection;
+
+    }
+
+    public void deleteStudentData() {
+        // call getConnection() method
+
+        //execute delete query
+    }
+
+    public void updateStudentData() {
+        // call getConnection() method
+
+        //execute update query
     }
 }
