2ece6c2797df3f208ddfce3cdc33f373421c1f5a
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-18 19:24:53 +0530

    reformatted the Student.java

--- src/main/java/com/jap/model/Student.java
+++ src/main/java/com/jap/model/Student.java
@@ -64,10 +64,6 @@ public class Student {
 
     @Override
     public String toString() {
-        return new StringJoiner(", ", Student.class.getSimpleName() + "[", "]")
-                .add("rollNumber=" + rollNumber)
-                .add("name='" + name + "'")
-                .add("grades=" + grades)
-                .toString();
+        return new StringJoiner(", ", Student.class.getSimpleName() + "[", "]").add("rollNumber=" + rollNumber).add("name='" + name + "'").add("grades=" + grades).toString();
     }
 }
