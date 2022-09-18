97274ee7944e439f82cd22a51b17056b456ef9d8
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-18 20:46:15 +0530

    changes done in student_database_queries.sql

--- src/main/resources/student_database_queries.sql
+++ src/main/resources/student_database_queries.sql
@@ -9,29 +9,57 @@ create table if not exists `school`.`student`
 (
    roll_number int not null primary key auto_increment,
    name varchar(25) not null,
-   grades int not null
+   grades int not null,
+   total_marks double not null
 );
 
 --Insert values into the table of student1:
-insert into `school`.`student`(`roll_number`,`name`,`grades`) VALUES
+insert into `school`.`student`(`roll_number`,`name`,`grades`,`total_marks`) values
 (
    '1',
    'John',
-   '85'
+   '85',
+   '654'
 );
 
 --Insert values into the table of student2:
-insert into `school`.`student`(`roll_number`,`name`,`grades`) VALUES
+insert into `school`.`student`(`roll_number`,`name`,`grades`,`total_marks`) values
 (
    '2',
    'Harry',
-   '89'
+   '89',
+   '667'
 );
 
 --Insert values into the table of student3:
-NSERT INTO `school`.`student`(`roll_number`,`name`,`grades`) VALUES
+insert into `school`.`student`(`roll_number`,`name`,`grades`,`total_marks`) values
 (
    '3',
    'Lydia',
-   '92'
+   '92',
+   '645'
 );
+--Insert values into the table of student4:
+insert into `school`.`student`(`roll_number`,`name`,`grades`,`total_marks`) values
+(
+   '14',
+   'Mary',
+   '90',
+   '665'
+);
+
+--Insert values into the table of student5:
+insert into `school`.`student` (`roll_number`, `name`, `grades`, `total_marks`) values
+(
+   '16',
+   'Johnny',
+   '91',
+   '678'
+);
+
+--deleted student details by roll number:
+delete from `school`.`student` where (`roll_number` = ?);
+
+--update roll_number by total_marks:
+update `school`.`student` set `roll_number` = '15' where (`roll_number` = '16');
+
