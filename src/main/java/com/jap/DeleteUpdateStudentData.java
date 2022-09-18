446730afc3cf26d6f21aa15d5c9ba3bf6a5f69a5
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-18 20:13:47 +0530

    created object using DatabaseService class and called the methods in it

--- src/main/java/com/jap/DeleteUpdateStudentData.java
+++ src/main/java/com/jap/DeleteUpdateStudentData.java
@@ -1,16 +1,27 @@
 package com.jap;
 
+import com.jap.service.database.DatabaseService;
+
 import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  * Hello world!
  */
 public class DeleteUpdateStudentData {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         DeleteUpdateStudentData salesDataDemo = new DeleteUpdateStudentData();
         salesDataDemo.deleteStudentData();
         salesDataDemo.updateStudentData();
+        // Creating an instance of the DatabaseService class.
+        DatabaseService databaseService = new DatabaseService();
+        try {
+            databaseService.connect();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        databaseService.printConnectionStatus();
     }
 
     public Connection getConnection() {
