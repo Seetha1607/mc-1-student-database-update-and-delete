6f332cea375fd046928865d24d78f9f98c11af41
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-18 20:04:40 +0530

    created a method to print the connection status

--- src/main/java/com/jap/service/database/DatabaseService.java
+++ src/main/java/com/jap/service/database/DatabaseService.java
@@ -8,6 +8,8 @@
 package com.jap.service.database;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseService {
     // Declaring a variable.
@@ -18,7 +20,7 @@ public class DatabaseService {
     private static final String PASSWORD = "Seetha@007";
 
     // A variable declaration.
-    private final Connection connection;
+    private Connection connection;
 
     public DatabaseService() {
         this.connection = null;
@@ -26,5 +28,33 @@ public class DatabaseService {
 
     public Connection getConnection() {
         return connection;
+    }
+
+    /**
+     * It connects to the database.
+     *
+     * @return A boolean value.
+     */
+    public boolean connect() throws ClassNotFoundException, SQLException {
+
+        // 1. load the jdbc driver into the program memory
+        // Class.forName("com.mysql.cj.jdbc.Driver");
+
+        // 2. create a connection object using the DriverManager class
+        connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+        // 3. check if the connection is successful
+        return connection != null;
+    }
+
+    /**
+     * If the connection is not null, print "Connected to the database", otherwise print "Not connected to the database"
+     */
+    public void printConnectionStatus() {
+        if (connection != null) {
+            System.out.println("Connected to the database");
+        } else {
+            System.err.println("Not connected to the database");
+        }
     }
 }
