40c3d043c0cde02e3ee237d40e855b6f94213a62
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-18 20:01:37 +0530

    created getter() method of connection object

--- src/main/java/com/jap/service/database/DatabaseService.java
+++ src/main/java/com/jap/service/database/DatabaseService.java
@@ -10,9 +10,21 @@ package com.jap.service.database;
 import java.sql.Connection;
 
 public class DatabaseService {
+    // Declaring a variable.
     private static final String URL = "jdbc:mysql://localhost:3306/school";
+    // Declaring a variable.
     private static final String USERNAME = "root";
+    // A variable declaration.
     private static final String PASSWORD = "Seetha@007";
 
-    private Connection connection;
+    // A variable declaration.
+    private final Connection connection;
+
+    public DatabaseService() {
+        this.connection = null;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 }
