7f92f96e3da8bb663bf2319303543f8599637b05
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-18 19:58:00 +0530

    created private field to store the reference of the connection object

--- src/main/java/com/jap/service/database/DatabaseService.java
+++ src/main/java/com/jap/service/database/DatabaseService.java
@@ -7,8 +7,12 @@
 
 package com.jap.service.database;
 
+import java.sql.Connection;
+
 public class DatabaseService {
     private static final String URL = "jdbc:mysql://localhost:3306/school";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "Seetha@007";
+
+    private Connection connection;
 }
